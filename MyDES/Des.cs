4432e6a8ca1a5945b14a8c5b4a6176e2b624648f
Osipov Konstantin Olegovich <83518160+keepdip@users.noreply.github.com>
2021-05-07 16:18:29 +0300

    Ver 1.0

--- MyDES/Des.cs
+++ MyDES/Des.cs
@@ -28,6 +28,7 @@ namespace MyDES
                         {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7, 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2, 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8, 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
                         };
 
+        //шифрование
         public static string DESEncryption(string message, string key)
         {
             while (message.Length % 8 != 0) message += (char)0;
@@ -54,6 +55,7 @@ namespace MyDES
             return BinaryToString(sb.ToString());
         }
 
+        //расшифрование
         public static string DESDecryption(string message, string key)
         {
             message = ConvertToBinary(message);
